04af9f281e72699c8bb9905ef6171e620ef3c34f
KeingCasabona <keing.limache.casabona@gmail.com>
2024-09-07 20:44:04 -0500

    feat: Add Drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,10 +9,82 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('SharedPreferences App'),
       ),
-      drawer: Container(
-        height: 100,
-        width: 100,
-        color: Colors.red,
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      'https://images.pexels.com/photos/2832382/pexels-photo-2832382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                ),
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CircleAvatar(
+                      radius: 28,
+                      backgroundImage: NetworkImage(
+                        'https://images.pexels.com/photos/1848565/pexels-photo-1848565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                      ),
+                    ),
+                    Text(
+                      'Fiorella Gonzales Tapia',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Administrador',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.people,
+                color: Colors.black.withOpacity(0.6),
+              ),
+              title: Text('Mi perfil'),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.file_copy,
+                color: Colors.black.withOpacity(0.6),
+              ),
+              title: Text('Portafolio General'),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.lock,
+                color: Colors.black.withOpacity(0.6),
+              ),
+              title: Text('Cambiar Contraseña'),
+            ),
+            Divider(
+              indent: 20,
+              endIndent: 20,
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.exit_to_app,
+                color: Colors.black.withOpacity(0.6),
+              ),
+              title: Text('Salir'),
+            ),
+          ],
+        ),
       ),
     );
   }
